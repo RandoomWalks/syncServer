0032b6499ce089025960277ce05901775e30daf4
RandoomWalks <ab4745802@gmail.com>
2024-07-02 05:20:16 -0700

    In Progress - Changes for heroku deploy (Has issue connecting to mongo atlas)

--- nestjs-service/src/database/database.module.ts
+++ nestjs-service/src/database/database.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './database.service';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
+
+
+// src/database/database.module.ts
+// import { Module } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { ConfigModule } from '@nestjs/config';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot(),
+//     MongooseModule.forRoot(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }),
+//   ],
+// })
+// export class DatabaseModule {}
--- nestjs-service/src/database/database.service.ts
+++ nestjs-service/src/database/database.service.ts
@@ -1,51 +1,10 @@
-
-// import { Injectable, Logger, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-// import { MongoClient, Db } from 'mongodb';
-
-// @Injectable()
-// export class DatabaseService implements OnModuleInit, OnModuleDestroy {
-//     private db: Db;
-//     private client: MongoClient;
-//     private readonly logger = new Logger(DatabaseService.name);
-
-//     async onModuleInit(): Promise<void> {
-//         const uri = process.env.DATABASE_URL || 'mongodb://mongo:27017/mydb';
-//         this.logger.log(`Connecting to MongoDB at ${uri}`);
-        
-//         try {
-//             this.client = new MongoClient(uri);
-//             await this.client.connect();
-//             this.db = this.client.db('mydb');
-//             this.logger.log('Successfully connected to MongoDB');
-//         } catch (error) {
-//             this.logger.error('Error connecting to MongoDB', error.stack);
-//             throw error;
-//         }
-//     }
-
-//     getDb(): Db {
-//         if (!this.db) {
-//             throw new Error('Database connection is not initialized.');
-//         }
-//         return this.db;
-//     }
-
-//     async onModuleDestroy(): Promise<void> {
-//         if (this.client) {
-//             this.logger.log('Closing MongoDB connection');
-//             try {
-//                 await this.client.close();
-//                 this.logger.log('MongoDB connection closed successfully');
-//             } catch (error) {
-//                 this.logger.error('Error closing MongoDB connection', error.stack);
-//             }
-//         }
-//     }
-// }
-
-
 import { Injectable, Logger, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { MongoClient, Db } from 'mongodb';
+import { ConfigService } from '@nestjs/config';
+import * as dns from 'dns';
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 
 @Injectable()
 export class DatabaseService implements OnModuleInit, OnModuleDestroy {
@@ -53,15 +12,29 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
     private client: MongoClient;
     private readonly logger = new Logger(DatabaseService.name);
 
+    constructor(private configService: ConfigService) {
+        // const uri = this.configService.get<string>('MONGO_URI');
+        const uri = "mongodb://ab4745802:E1XuVmfq825LfV9o@cluster0-shard-00-00.mongodb.net:27017,cluster0-shard-00-01.mongodb.net:27017,cluster0-shard-00-02.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+        this.client = new MongoClient(uri);
+        // this.client = new MongoClient(this.configService.get<string>('MONGO_URI'));
+    }
+
+
+
     async onModuleInit(): Promise<void> {
-        const uri = process.env.DATABASE_URL || 'mongodb://mongo:27017/mydb';
+        // const uri = process.env.DATABASE_URL || 'mongodb://mongo:27017/mydb';
+        // const uri = this.configService.get<string>('MONGO_URI');
+        const uri  ="mongodb://ab4745802:E1XuVmfq825LfV9o@cluster0-shard-00-00.mongodb.net:27017,cluster0-shard-00-01.mongodb.net:27017,cluster0-shard-00-02.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+
         this.logger.log(`Connecting to MongoDB at ${uri}`);
-        
+
         try {
-            this.client = new MongoClient(uri);
+            // this.client = new MongoClient(uri);
             await this.client.connect();
             this.db = this.client.db('mydb');
             this.logger.log('Successfully connected to MongoDB');
+            console.log('Connected to MongoDB');
 
             // Ensure indexes are created
             await this.createIndexes();
@@ -96,3 +69,32 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
         await collection.createIndex({ 'data._id': 1 });
     }
 }
+
+
+// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { MongoClient } from 'mongodb';
+// import { ConfigService } from '@nestjs/config';
+
+// @Injectable()
+// export class DatabaseService implements OnModuleInit, OnModuleDestroy {
+//     private client: MongoClient;
+//     private db: any;
+
+//     constructor(private configService: ConfigService) {
+//         this.client = new MongoClient(this.configService.get<string>('MONGO_URI'));
+//     }
+
+//     async onModuleInit() {
+//         await this.client.connect();
+//         this.db = this.client.db();
+//         console.log('Connected to MongoDB');
+//     }
+
+//     getDb() {
+//         return this.db;
+//     }
+
+//     async onModuleDestroy() {
+//         await this.client.close();
+//     }
+// }
